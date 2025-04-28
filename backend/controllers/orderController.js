9de37cc9c7f64f254791201e52cbe980c68fcbaa
Sara Mediouni <81996794+Sara-Mediouni@users.noreply.github.com>
2025-04-28 22:51:14 +0100

    readme changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,7 +3,7 @@ const userModel = require("../models/UserModel");
 const Stripe = require("stripe");
 
 const stripe = new Stripe(
-  "sk_test_51RBMXo4FlSbelSuKp15qkCFl65CTopoEuNZcQ5pIjYp0rJjoHazQIQkaunaimiOvL6enfref6slxKJRmfvx11w1q003oe4GUPp"
+  process.env.STRIPE_SECRET_KEY
 );
 //placing user order for frontend
 const placeOrder = async (req, res) => {
